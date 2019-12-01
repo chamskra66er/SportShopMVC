7d9e80a9df5cf8004ab6957a3571373371f8d935
Anatoliy Novikov <chamskra66er@gmail.com>
2019-12-01 12:01:05 +0300

    Добавьте файлы проекта.

---
+++ SportsStore/Models/Order.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace SportsStore.Models {
+
+    public class Order {
+
+        [BindNever]
+        public int OrderID { get; set; }
+        [BindNever]
+        public ICollection<CartLine> Lines { get; set; }
+
+        [BindNever]
+        public bool Shipped { get; set; }
+
+        [Required(ErrorMessage = "Пожалуйста, введите имя")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Пожалйуста, введите адрес")]
+        public string Line1 { get; set; }
+        public string Line2 { get; set; }
+        public string Line3 { get; set; }
+
+        [Required(ErrorMessage = "Пожалуйста, введите название города")]
+        public string City { get; set; }
+
+        [Required(ErrorMessage = "Пожалйуста, введите название района")]
+        public string State { get; set; }
+
+        public string Zip { get; set; }
+
+        [Required(ErrorMessage = "Пожалуйста, введите название страны")]
+        public string Country { get; set; }
+
+        public bool GiftWrap { get; set; }
+    }
+}
---
+++ SportsStore/Models/Product.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace SportsStore.Models {
+
+    public class Product {
+        public int ProductID { get; set; }
+
+        [Required(ErrorMessage = "Пожалуйста, введите товар")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Пожалуйста, введите описание")]
+        public string Description { get; set; }
+
+        [Required]
+        [Range(0.01, double.MaxValue,
+            ErrorMessage = "Пожалуйста, введите корректную цену")]
+        public decimal Price { get; set; }
+
+        [Required(ErrorMessage = "Пожалуйста, укажите категорию")]
+        public string Category { get; set; }
+    }
+}
