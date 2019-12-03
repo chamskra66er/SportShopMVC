edd41dffd810285c60336c65de500d8db194db14
Anatoliy Novikov <chamskra66er@gmail.com>
2019-12-03 22:45:58 +0300

    started (PatronController)

---
+++ SportsStore/Controllers/PatronController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
+
+namespace SportsStore.Controllers
+{
+    public class PatronController:Controller
+    {
+        private readonly AppLoyaltyDbContext _context;
+        public PatronController(AppLoyaltyDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View("Index");
+        }
+    }
+}
